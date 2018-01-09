cbb1a70015c720373b1d9e2c81fe23d39934805e
GiraffeTree <15355498770@163.com>
2018-01-09 18:11:22 +0800

    Create action.php

---
+++ web/action.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/8
+ * Time: 12:57
+ */
+
+class Action
+{
+    public static function curl_get($url)
+    {
+
+        $testurl = $url;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $testurl);
+        //参数为1表示传输数据，为0表示直接输出显示。
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //参数为0表示不带头文件，为1表示带头文件
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
+    /*
+     * url:访问路径
+     * array:要传递的数组
+     * */
+    public static function curl_post($url, $array)
+    {
+
+        $curl = curl_init();
+        //设置提交的url
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //设置头文件的信息作为数据流输出
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        //设置获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        //设置post方式提交
+        curl_setopt($curl, CURLOPT_POST, 1);
+        //设置post数据
+        $post_data = $array;
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+        //执行命令
+        $data = curl_exec($curl);
+        //关闭URL请求
+        curl_close($curl);
+        //获得数据并返回
+        return $data;
+    }
+
+
+    //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);//这个是重点。
+
+    /**
+     * @param $url
+     * @return mixed
+     */
+    public static function curl_get_https($url)
+    {
+
+        $testurl = $url;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $testurl);
+        //参数为1表示传输数据，为0表示直接输出显示。
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //参数为0表示不带头文件，为1表示带头文件
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);//这个是重点。
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
+
+    /*
+    * url:访问路径
+    * data:要传递的数组
+    * */
+
+    public static function curl_post_https($url, $data)
+    { // 模拟提交数据函数
+        $curl = curl_init(); // 启动一个CURL会话
+        $data_string = json_encode($data);
+        curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2); // 从证书中检查SSL加密算法是否存在
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+        curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string); // Post提交的数据包
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string))
+        );
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+        $tmpInfo = curl_exec($curl); // 执行操作
+        if (curl_errno($curl)) {
+            echo 'Errno' . curl_error($curl);//捕抓异常
+        }
+        curl_close($curl); // 关闭CURL会话
+        return $tmpInfo; // 返回数据，json格式
+    }
+
+}
